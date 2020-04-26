b35385e9044537c57bb80cb4948eccae98f52725
kondr1 <i@kondr1.ru>
2020-04-26 12:29:16 +0300

    fix

--- src/discord.js
+++ src/discord.js
@@ -32,7 +32,7 @@ async function createTeamRoleIfNotExist (teamName, ctx) {
 }
 
 async function createRoleIfNotExist (role, { guild }) {
-  const currentRole = guild.roles.find(r => r.name === role.name)
+  const currentRole = guild.roles.cache.find(r => r.name === role.name)
   console.log(`Create role '${role.name}' ${currentRole && currentRole[0]} `)
   if (currentRole && currentRole[0]) {
     return currentRole[0]
