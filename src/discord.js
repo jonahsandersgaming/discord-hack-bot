986777067163a225fbe5179c6b2100d5da41679c
kondr1 <i@kondr1.ru>
2020-04-26 11:03:53 +0300

    log

--- src/discord.js
+++ src/discord.js
@@ -100,9 +100,11 @@ client.on('guildMemberAdd', async member => {
   //     }
   //   }
   console.log(`Wild ${member.user.username} appeard`)
+  console.dir(member.user)
 
   const user = await (await mongo()).getUserByDiscordId(member.user.username) // TODO: наверное корректнее проверять по id
   const roles = []
+  console.dir(user)
   if (user && (!user.admin && !user.mentor)) {
     roles.push(await createRoleIfNotExist(memberRole, member)) // роль учасника
   }
